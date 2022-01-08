82fa0a5698a062e929bc8ef18121609482ebffbb
Tyrope <github@tyrope.nl>
2022-01-08 14:13:17 +0100

    Added achievement-unlocking scripts; THESE CONTAIN SPOILERS, OBVIOUSLY.

---
+++ chievos/whileTrue.js
@@ -0,0 +1,14 @@
+/** @param {NS} ns **/
+
+///////////////////
+///// WARNING /////
+///////////////////
+// This script will freeze your game.
+// the only way to recover is to restart the game with ALL SCRIPTS KILLED.
+// So you might wanna run this just before you augment or something.
+
+export async function main(ns) {
+    while(true){
+        // Get achievement.
+    }
+}
