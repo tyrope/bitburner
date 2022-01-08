b4266eb90fc9d2eb216a1410c10c5170c133f7eb
Tyrope <github@tyrope.nl>
2022-01-08 13:47:12 +0100

    [sort, srvMngr]Migrate to NS2

---
+++ sort.js
@@ -0,0 +1,52 @@
+/** @param {NS} ns **/
+
+function checkIfTodo(ns, server) {
+    // TODO change below to the effective hacking level required.
+    let lvl = ns.getServerRequiredHackingLevel(server);
+
+    if (lvl < ns.getHackingLevel()) {
+        let isHacking = false;
+        let processes = ns.ps(server);
+        for (let p = 0; p < processes.length; p++) {
+            if (processes[p].filename == "basicHack.js") {
+                isHacking = true;
+                break;
+            }
+        }
+        if (isHacking == false && lvl > 1) {
+            print(server + " requires level " + lvl);
+        }
+    }
+}
+
+export async function main(ns) {
+    let scanned = [];
+    let frontier = ["home"];
+    let todo; let neighbors;
+
+
+    ns.disableLog('scan');
+    ns.disableLog('getHackingLevel');
+    ns.disableLog('getServerRequiredHackingLevel');
+    ns.clearLog();
+
+    // Find all servers.
+    while (frontier.length > 0) {
+        // Get the next server.
+        todo = frontier.shift()
+        // check if we need to hack this one.
+        checkIfTodo(ns, todo);
+        // Tell the script we've scanned it.
+        scanned.push(todo);
+        // Go through it's neighbors.
+        neighbors = ns.scan(todo);
+        for (var i = 0; i < neighbors.length; i++) {
+            // Check if we've scanned it.
+            if (scanned.indexOf(neighbors[i]) == -1) {
+                // Add it to the list if not.
+                frontier.push(neighbors[i]);
+            }
+        }
+    }
+}
+
---
+++ srvMngr.js
@@ -0,0 +1,48 @@
+// Server Manager (c) 2022 Tyrope
+// Usage: run srvMngr.js [RAM] [mode]
+// parameter RAM: Amount of RAM.
+// parameter mode:
+//  1 = Delete servers small than the RAM.
+//  2 = Check price of 1 server with RAM.
+//  Anything else = Buy max amount of servers with RAM.
+
+function deleteServers(ns) {
+    let srvs = ns.getPurchasedServers();
+    for (let i = 0; i < srvs.length; i++) {
+        if (ns.getServerMaxRam(srvs[i]) < ns.args[0]) {
+            ns.killall(srvs[i]);
+            ns.deleteServer(srvs[i]);
+        }
+    }
+}
+
+async function purchase(ns) {
+    while (getPurchasedServers().length < getPurchasedServerLimit()) {
+        if (price(ns) < ns.getServerMoneyAvailable("home")) {
+            ns.purchaseServer("srv", args[0]);
+        }
+    }
+}
+
+function price(ns) {
+    return ns.getPurchasedServerCost(ns.args[0]);
+}
+
+export async function main(ns) {
+    if (ns.args[0] === undefined) {
+        ns.tprint("No RAM amount provided.");
+        ns.exit();
+    }
+
+    switch (ns.args[1]) {
+        case 1:
+            deleteServers(ns);
+            break;
+        case 2:
+            ns.tprint(price(ns));
+            break;
+        default:
+            await purchase(ns);
+    }
+}
+
