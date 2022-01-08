f9dd81453b8fdda4f11dd4b6aa98f52c67656823
Tyrope <github@tyrope.nl>
2022-01-08 12:25:14 +0100

    [serverGrower]migrated to NS2

---
+++ serverGrower.js
@@ -0,0 +1,25 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    let tgt = "";
+    if(ns.args[0] == undefined){
+        tprint("No target specified.");
+        ns.exit();
+    }else {
+        tgt = ns.args[0];
+    }
+
+    // Infinite loop, go!
+    while (true) {
+        // Make sure we're not fighting unneeded security.
+        while (ns.getServerSecurityLevel(tgt) > ns.getServerMinSecurityLevel(tgt)) {
+            await ns.weaken(tgt);
+        }
+        if (ns.getServerMoneyAvailable(tgt) < ns.getServerMaxMoney(tgt)) {
+            await ns.grow(tgt);
+        } else {
+            break;
+        }
+    }
+    ns.tprint(ns.getHostname() + " completed growing " + tgt);
+}
+
