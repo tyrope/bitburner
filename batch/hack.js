a413fd5b4fe4a47c9b114815cc2efd141ad64f40
Tyrope <github@tyrope.nl>
2022-01-09 14:53:14 +0100

    [batch/hack]Print the money hacked.

--- batch/hack.js
+++ batch/hack.js
@@ -4,5 +4,11 @@ export async function main(ns) {
         ns.tprint("usage: target:string, startAt:number");
     }
     await ns.sleep(ns.args[1] - ns.getTimeSinceLastAug());
-    await ns.hack(ns.args[0]);
+    let profit = await ns.hack(ns.args[0]);
+
+    while(profit == 0){
+        await sleep(100);
+    }
+    ns.tprint(`Hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}`);
 }
+
