cf24b93c70b884c74ff2b5e223f5c5f670463577
Tyrope <github@tyrope.nl>
2022-01-10 20:53:43 +0100

    [batch/hack]Toast instead of tprint.

--- batch/hack.js
+++ batch/hack.js
@@ -1,14 +1,11 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    if (ns.args[1] === undefined) {
-        ns.tprint("usage: target:string, startAt:number");
-    }
     await ns.sleep(ns.args[1] - ns.getTimeSinceLastAug());
     let profit = await ns.hack(ns.args[0]);
 
-    while(profit == 0){
+    while (profit == 0) {
         await sleep(100);
     }
-    ns.tprint(`SUCCESS: Hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}`);
+    ns.toast(`Hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}`, 'success');
 }
 
