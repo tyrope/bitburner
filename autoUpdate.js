9356b10841679f0195ab3c72c99ecda42f98a8fa
Tyrope <github@tyrope.nl>
2022-01-10 19:49:52 +0100

    [autoUpdate]Added catch for when a file can't be downloaded; make sure scripts aren't running so we can overwrite them; only allow to run on home.

--- autoUpdate.js
+++ autoUpdate.js
@@ -4,7 +4,22 @@ let timeOut = 1000 * 60 * 1; // One minute on the download timeouts.
 
 async function getFile(ns, fileName) {
     let timeStart = ns.getTimeSinceLastAug();
-    let downloaded = await ns.wget(repo + fileName, fileName);
+
+    // Subfoldered files need their slashy prefix because "reasons"
+    if (fileName.contains('/')) fileName = '/' + fileName;
+
+    let downloaded;
+
+    if (ns.scriptRunning(fileName, ns.getHostname())) {
+        ns.kill(fileName);
+    }
+
+    try {
+        downloaded = await ns.wget(repo + fileName, fileName);
+    } catch (e) {
+        ns.toast("Failed to update " + fileName, "error");
+        return false;
+    }
 
     // Wait for the timeout...
     while (ns.getTimeSinceLastAug() - timeStart < timeOut) {
@@ -18,14 +33,15 @@ async function getFile(ns, fileName) {
             await ns.sleep(1000);
         }
     }
-
-    // This is bad, sorta.
-    ns.toast("Failed to update file: " + fileName, "error");
-    return false;
 }
 
 export async function main(ns) {
     ns.disableLog("sleep");
+
+    if (ns.getHostname() != "home") {
+        ns.tprint("FAIL: Run this script on home.");
+        ns.exit();
+    }
 
     // Get the list of files to auto-update.
     let timeStart = ns.getTimeSinceLastAug();
@@ -48,7 +64,7 @@ export async function main(ns) {
     // Read the list of files and pdate all of them.
     let files = ns.read("fileNames.txt");
     for (let file of files.split("\n")) {
-        if(file == ""){
+        if (file == "") {
             continue;
         }
         await getFile(ns, file);
