0c27c2d68cdeca320375b9920b58f6be28ed9f6a
Tyrope <github@tyrope.nl>
2022-01-10 22:50:24 +0100

    [srvMngr]We can now name the servers we buy, and are not limited to the maximum amount.

--- srvMngr.js
+++ srvMngr.js
@@ -2,32 +2,41 @@
 // Usage: run srvMngr.js [RAM] [mode]
 // parameter RAM: Amount of RAM.
 // parameter mode:
+//  0 = Buy servers with RAM.
 //  1 = Delete servers small than the RAM.
 //  2 = Check price of 1 server with RAM.
-//  Anything else = Buy max amount of servers with RAM.
+// parameter amount: Amount of server(s) to buy.
+// parameter name: Name of server(s) to buy.
+
 
 function deleteServers(ns) {
+    let del = 0;
     let srvs = ns.getPurchasedServers();
     for (let i = 0; i < srvs.length; i++) {
         if (ns.getServerMaxRam(srvs[i]) < ns.args[0]) {
             ns.killall(srvs[i]);
             ns.deleteServer(srvs[i]);
+            del++;
         }
     }
+    ns.tprint(`Deleted ${del} servers.`);
 }
 
 async function purchase(ns) {
-    while (ns.getPurchasedServers().length < ns.getPurchasedServerLimit()) {
-        if (price(ns) < ns.getServerMoneyAvailable("home")) {
-            ns.purchaseServer("srv", ns.args[0]);
-        } else {
-            await ns.sleep(1000);
+    for (i = 0; i < ns.args[2]; i++) {
+        while (true) {
+            if (price(ns) < ns.getServerMoneyAvailable("home")) {
+                ns.tprint(`bought ${ns.purchaseServer(ns.args[3], ns.args[0])}`);
+                continue;
+            } else {
+                await ns.sleep(1000);
+            }
         }
     }
 }
 
 function price(ns) {
-    return ns.getPurchasedServerCost(ns.args[0]);
+    ns.tprint(ns.getPurchasedServerCost(ns.args[0]));
 }
 
 export async function main(ns) {
@@ -37,14 +46,17 @@ export async function main(ns) {
     }
 
     switch (ns.args[1]) {
+        case 0:
+            await purchase(ns);
+            break;
         case 1:
             deleteServers(ns);
             break;
         case 2:
-            ns.tprint(price(ns));
+            price(ns);
             break;
         default:
-            await purchase(ns);
+            ns.tprint("Wrong parameters.");
     }
 }
 
