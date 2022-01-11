ff1efca85178f0f8f5c876151845a4b4467bce32
Tyrope <github@tyrope.nl>
2022-01-11 01:22:32 +0100

    [srvMngr]Instead of buying everything, buy only the amount we want. Oh and jiggle the parameters around. (Better documentation Soon:TM:)

--- srvMngr.js
+++ srvMngr.js
@@ -2,20 +2,23 @@
 // Usage: run srvMngr.js [RAM] [mode]
 // parameter RAM: Amount of RAM.
 // parameter mode:
-//  0 = Buy servers with RAM.
-//  1 = Delete servers small than the RAM.
-//  2 = Check price of 1 server with RAM.
+//  0 = Check price of 1 server with RAM.
+//  1 = Delete IDLE servers small than the RAM.
+//  2 = Delete ALL servers small than the RAM.
+//  3 = Buy servers with RAM.
 // parameter amount: Amount of server(s) to buy.
 // parameter name: Name of server(s) to buy.
 
 
-function deleteServers(ns) {
+function deleteServers(ns, delAll) {
     let del = 0;
     let srvs = ns.getPurchasedServers();
     for (let i = 0; i < srvs.length; i++) {
         if (ns.getServerMaxRam(srvs[i]) < ns.args[0]) {
-            ns.killall(srvs[i]);
-            ns.deleteServer(srvs[i]);
+            if (delAll || ns.getServerUsedRam(srvs[i]) == 0) {
+                ns.killall(srvs[i]);
+                ns.deleteServer(srvs[i]);
+            }
             del++;
         }
     }
@@ -23,11 +26,12 @@ function deleteServers(ns) {
 }
 
 async function purchase(ns) {
-    for (i = 0; i < ns.args[2]; i++) {
+    for (let i = 0; i < ns.args[2]; i++) {
         while (true) {
-            if (price(ns) < ns.getServerMoneyAvailable("home")) {
-                ns.tprint(`bought ${ns.purchaseServer(ns.args[3], ns.args[0])}`);
-                continue;
+            if (ns.getPurchasedServerCost(ns.args[0]) < ns.getServerMoneyAvailable("home")) {
+                let ret = ns.purchaseServer(ns.args[3], ns.args[0]);
+                ns.tprint(`bought ${ret}`);
+                break;
             } else {
                 await ns.sleep(1000);
             }
@@ -36,24 +40,28 @@ async function purchase(ns) {
 }
 
 function price(ns) {
-    ns.tprint(ns.getPurchasedServerCost(ns.args[0]));
+    ns.tprint(`A server with ${ns.nFormat(ns.args[0] * 10e8, "0.0b")}RAM costs ${ns.nFormat(ns.getPurchasedServerCost(ns.args[0]), "0.0a")}`);
 }
 
 export async function main(ns) {
-    if (ns.args[0] === undefined) {
-        ns.tprint("No RAM amount provided.");
+    if (ns.args[1] === undefined) {
+        ns.tprint("Wrong parameters.");
         ns.exit();
+        return;
     }
 
     switch (ns.args[1]) {
         case 0:
-            await purchase(ns);
+            price(ns);
             break;
         case 1:
-            deleteServers(ns);
+            deleteServers(ns, false);
             break;
         case 2:
-            price(ns);
+            deleteServers(ns, true);
+            break;
+        case 3:
+            await purchase(ns);
             break;
         default:
             ns.tprint("Wrong parameters.");
