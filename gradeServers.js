fd1d8a5da0db8b8c148543f9d84d9162b7e65cd4
Tyrope <github@tyrope.nl>
2022-01-11 22:31:34 +0100

    [gradeServers]Implement a TopX view, tweak formulas usage, print to the log window.

--- gradeServers.js
+++ gradeServers.js
@@ -3,9 +3,13 @@ import { getBatchInfo } from 'batchDaemon.js'
 /** @param {NS} ns **/
 export async function main(ns) {
     ns.disableLog('ALL');
+    ns.tail();
 
     let pct = ns.args[0] ? ns.args[0] : 20;
     let verbose = ns.args[1] ? ns.args[0] : false;
+    let topOnly = ns.args[2] ? ns.args[2] + 1 : Infinity;
+
+    let useFormulas = ns.fileExists("Formulas.exe", "home");
 
     // Get all the servers.
     let servers = scanServers(ns);
@@ -16,14 +20,12 @@ export async function main(ns) {
             // have money.
             ns.getServerMaxMoney(s) != 0 &&
             // are below or equal to our hacking level.
-            ns.getServerRequiredHackingLevel(s) <= ns.getHackingLevel() &&
-            // Aren't in the blacklist.
-            !["n00dles"].includes(s)
+            ns.getServerRequiredHackingLevel(s) <= ns.getHackingLevel()
         )
     });
 
     // Sort by score.
-    servers.sort((a, b) => { return getServerScore(ns, b, pct) - getServerScore(ns, a, pct); });
+    servers.sort((a, b) => { return getServerScore(ns, b, pct, useFormulas) - getServerScore(ns, a, pct, useFormulas); });
 
     let data;
     if (verbose) {
@@ -33,14 +35,17 @@ export async function main(ns) {
     }
 
     for (let server of servers) {
-        data.push(getServerInfo(ns, server, pct, verbose));
+        if (data.length >= topOnly) {
+            break;
+        }
+        data.push(getServerInfo(ns, server, pct, verbose, useFormulas));
     }
-    ns.tprint("\n" + makeTable(data, false));
+    ns.print(makeTable(data, false));
 }
 
-function getServerScore(ns, server, pct) {
+function getServerScore(ns, server, pct, useFormulas) {
     let chanceToHack;
-    if (ns.fileExists("Formulas.exe", "home")) {
+    if (useFormulas) {
         let srv = ns.getServer(server);
         srv.hackDifficulty = srv.minDifficulty;
         srv.moneyAvailable = srv.moneyMax;
@@ -48,33 +53,42 @@ function getServerScore(ns, server, pct) {
     } else {
         chanceToHack = ns.hackAnalyzeChance(server);
     }
-    let batchInfo = getBatchInfo(ns, server, pct);
+    let batchInfo = getBatchInfo(ns, server, pct, useFormulas);
     let moneyPerSec = batchInfo[2] / batchInfo[1];
     return (moneyPerSec * chanceToHack) / (batchInfo[0] * 0.25);
 }
 
-function getServerInfo(ns, server, pct, verbose) {
-    let batchInfo = getBatchInfo(ns, server, pct);
+function getServerInfo(ns, server, pct, verbose, useFormulas) {
+    let batchInfo = getBatchInfo(ns, server, pct, useFormulas);
     if (verbose) {
         let maxMoney = ns.getServerMaxMoney(server);
         let growth = ns.getServerGrowth(server);
+        let chanceToHack;
+        if (useFormulas) {
+            let srv = ns.getServer(server);
+            srv.hackDifficulty = srv.minDifficulty;
+            srv.moneyAvailable = srv.moneyMax;
+            chanceToHack = ns.formulas.hacking.hackChance(srv, ns.getPlayer());
+        } else {
+            chanceToHack = ns.hackAnalyzeChance(server);
+        }
         return ([
             server,
             ns.nFormat(maxMoney, "0.00a"),
             growth,
             ns.getServerMinSecurityLevel(server),
-            `${Math.round(ns.hackAnalyzeChance(server) * 100)}%`,
+            `${ns.nFormat(chanceToHack * 100, "0.00")}%`,
             `${ns.nFormat(maxMoney * growth, "0.00a")}`,
             ns.nFormat(batchInfo[2] / batchInfo[1], "0.00a"),
             ns.nFormat(batchInfo[0] * 1e9, "0.00b"),
-            ns.nFormat(getServerScore(ns, server, pct), "0.000")
+            ns.nFormat(getServerScore(ns, server, pct, useFormulas), "0.000")
         ]);
     } else {
         return ([
             server,
             ns.nFormat(batchInfo[2] / batchInfo[1], "0.000a"),
             ns.nFormat(batchInfo[0] * 1e9, "0.00b"),
-            ns.nFormat(getServerScore(ns, server, pct), "0.000")
+            ns.nFormat(getServerScore(ns, server, pct, useFormulas), "0.000")
         ]);
     }
 }
