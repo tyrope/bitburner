540ebeaf023c2d348bf98036ad8a8d740291072d
Tyrope <github@tyrope.nl>
2022-01-11 19:00:14 +0100

    [batch/hack] It's okay to not hack anything, it happens sometimes.

--- batch/hack.js
+++ batch/hack.js
@@ -3,12 +3,15 @@ export async function main(ns) {
     await ns.sleep(ns.args[1] - ns.getTimeSinceLastAug());
     let profit = await ns.hack(ns.args[0]);
 
-    if (Math.abs(profit - ns.args[2]) > 1000) {
+    if (profit == 0) {
+        // Fail! :(
+        ns.toast(`Hacked against ${ns.args[0]} failed...`, 'warning');
+    } else if (Math.abs(profit - ns.args[2]) > 1000) {
         // We hacked a different amount than expected.
-        ns.tprint(`ERROR: ${ns.getHostname()} hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}, expected ${ns.nFormat(ns.args[1], "0.0a")}`);
+        ns.tprint(`ERROR: ${ns.getHostname()} hacked \$${ns.nFormat(profit, "0.000a")} from ${ns.args[0]}, expected ${ns.nFormat(ns.args[2], "0.000a")}`);
     } else {
         // success!
-        ns.toast(`Hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}`, 'success');
+        ns.toast(`Hacked \$${ns.nFormat(profit, "0.000a")} from ${ns.args[0]}`, 'success');
     }
 }
 
