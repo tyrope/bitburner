8a272a91de666bdb5ac6e468627be9183607f349
Tyrope <github@tyrope.nl>
2022-01-11 21:24:42 +0100

    [batch/hack]If we're hacking more than expected, that's a warning, not an error.

--- batch/hack.js
+++ batch/hack.js
@@ -6,9 +6,17 @@ export async function main(ns) {
     if (profit == 0) {
         // Fail! :(
         ns.toast(`Hacked against ${ns.args[0]} failed...`, 'warning');
-    } else if (Math.abs(profit - ns.args[2]) > 1000) {
+        return;
+    } else if (Math.abs((profit - ns.args[2]) / ((profit + ns.args[2]) / 2)) > 0.05) {
         // We hacked a different amount than expected.
-        ns.tprint(`ERROR: ${ns.getHostname()} hacked \$${ns.nFormat(profit, "0.000a")} from ${ns.args[0]}, expected ${ns.nFormat(ns.args[2], "0.000a")}`);
+        if (profit < ns.args[2]) {
+            // Not enough.
+            ns.tprint(`ERROR: ${ns.getHostname()} hacked \$${ns.nFormat(profit, "0.000a")} from ${ns.args[0]}, expected ${ns.nFormat(ns.args[2], "0.000a")}`);
+        } else {
+            // Too much.
+            ns.tprint(`WARNING: ${ns.getHostname()} overhacked ${ns.args[0]} by \$${ns.nFormat((profit/ns.args[2])*100-100, "0.00%")}`);
+
+        }
     } else {
         // success!
         ns.toast(`Hacked \$${ns.nFormat(profit, "0.000a")} from ${ns.args[0]}`, 'success');
