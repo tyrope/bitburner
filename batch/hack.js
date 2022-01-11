9462265a1c3224f4bf616ff3b6685db7659f1afe
Tyrope <github@tyrope.nl>
2022-01-11 17:12:34 +0100

    [batch/hack]We don't need that delay loop; we do however, should check against the money, not the delay.

--- batch/hack.js
+++ batch/hack.js
@@ -3,11 +3,7 @@ export async function main(ns) {
     await ns.sleep(ns.args[1] - ns.getTimeSinceLastAug());
     let profit = await ns.hack(ns.args[0]);
 
-    while (profit == 0) {
-        await ns.sleep(100);
-    }
-
-    if (Math.abs(profit - ns.args[1]) > 1000) {
+    if (Math.abs(profit - ns.args[2]) > 1000) {
         // We hacked a different amount than expected.
         ns.tprint(`ERROR: ${ns.getHostname()} hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}, expected ${ns.nFormat(ns.args[1], "0.0a")}`);
     } else {
