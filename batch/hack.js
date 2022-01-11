172709bafa3beed1d0911ceab99932ad2de39921
Tyrope <github@tyrope.nl>
2022-01-11 16:34:18 +0100

    [batch/hack]let's keep that error around...

--- batch/hack.js
+++ batch/hack.js
@@ -6,9 +6,13 @@ export async function main(ns) {
     while (profit == 0) {
         await ns.sleep(100);
     }
-    if(profit == ns.args[1]){
+
+    if (Math.abs(profit - ns.args[1]) > 1000) {
+        // We hacked a different amount than expected.
+        ns.tprint(`ERROR: ${ns.getHostname()} hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}, expected ${ns.nFormat(ns.args[1], "0.0a")}`);
+    } else {
+        // success!
         ns.toast(`Hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}`, 'success');
-    }else{
-        ns.toast(`Hacked \$${ns.nFormat(profit, "0.0a")} from ${ns.args[0]}, expected ${ns.nFormat(ns.args[1], "0.0a")}`, 'error');
     }
 }
+
