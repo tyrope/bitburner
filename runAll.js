6142d0ea6cf5fd2fb2d66d9a7c35d5e1dfa0260b
Tyrope <github@tyrope.nl>
2022-01-19 16:36:31 +0100

    [runAll]Use an argument instead of a hardcoded script; and make arguments optional.

--- runAll.js
+++ runAll.js
@@ -1,10 +1,12 @@
 // This script goes through *all* servers in the game and runs scriptName on them with max RAM.
-// Usage: run runAll.js target
-//parameter target: exact name (case sensitive, maybe) to grow.
+// Usage: run runAll.js [script] (args)
+// parameter script: The script to run on the target servers.
+// parameter args: Arguments to pass onto the script specified.
 
 import { getServers } from '/lib/netLib.js'
 
-let scriptName = "serverGrower.js";
+let scriptName;
+let scriptArgs;
 
 export function autocomplete(data, args) {
     return [...data.servers];
@@ -91,21 +93,34 @@ async function control(ns, server, tools) {
     let threads = Math.floor((ns.getServerMaxRam(server) - ns.getServerUsedRam(server)) / ns.getScriptRam(scriptName))
     // run the server grower, if we can.
     if (threads > 0) {
-        ns.exec(scriptName, server, threads, ns.args[0]);
+        ns.exec(scriptName, server, threads, ...scriptArgs);
     }
 }
 
 /** @param {NS} ns **/
 export async function main(ns) {
     if (ns.args[0] == undefined) {
-        ns.tprint("No target specified.");
+        ns.tprint("No script specified.");
         ns.exit();
+    }
+    scriptName = ns.args[0];
+
+    if (ns.fileExists(scriptName, 'home') == false) {
+        ns.tprint("Script not found.");
+        ns.exit();
+    }
+
+    if (ns.args.length > 1) {
+        scriptArgs = ns.args.slice(1);
+    } else {
+        scriptArgs = Array();
     }
 
     let tools = getTools(ns);
 
     //TODO: Allow for blacklisting.
-    for (let server in getServers(ns)) {
+    for (let server of getServers(ns)) {
+        ns.print(server);
         await control(ns, server, tools);
     }
 
