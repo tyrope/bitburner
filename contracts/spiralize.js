05826e3fb602dfa5017b8fa878fa78d01641a01e
Tyrope <github@tyrope.nl>
2022-01-25 18:36:23 +0100

    [spiralize]New type of contract found. SOLVER NOT IMPLEMENTED!

---
+++ contracts/spiralize.js
@@ -0,0 +1,14 @@
+/* Spiralize Matrix
+Given an array of arrays of numbers representing a 2D matrix, return the elements of the matrix as an array in spiral order.
+Note that the matrix will not always be square.
+*/
+
+/** @param {Number[][]} input */
+export function solver(input) {
+    //NOPE.
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    ns.tprint(solver(JSON.parse(ns.args[0])));
+}
