14794e514268a68f2f04fff8688b669c4f9072b2
Tyrope <github@tyrope.nl>
2022-01-25 18:34:57 +0100

    [minSumTriangle]Standardize variable name, remove unneeded whitespace.

--- contracts/minSumTriangle.js
+++ contracts/minSumTriangle.js
@@ -1,23 +1,17 @@
 /* Minimum Path Sum in a Triangle
-
 Given a triangle, find the minimum path sum from top to bottom.
 In each step of the path, you may only move to adjacent numbers in the row below.
 The triangle is represented as a 2D array of numbers.
 */
 
-
-
-
-/**
- * @param {Array} triangle
- */
-export function solver(triangle) {
+/** @param {Number[]} input */
+export function solver(input) {
     let minCost = Number.POSITIVE_INFINITY;
-    const rows = triangle.length;
+    const rows = input.length;
 
     // Go through all possible routes.
     for (let i = 0; i < 2 ** (rows - 1); i++) {
-        let costSoFar = triangle[0][0];
+        let costSoFar = input[0][0];
         let column = 0;
         const route = i.toString(2).padStart(rows - 1, "0");
 
@@ -26,7 +20,7 @@ export function solver(triangle) {
             if (route[step - 1] == "1") {
                 column++;
             }
-            costSoFar += triangle[step][column];
+            costSoFar += input[step][column];
         }
 
         // Is this the cheapest route so far?
