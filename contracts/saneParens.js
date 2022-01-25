4979022e7e92c562f0bc91583f4920f2ffacffa5
Tyrope <github@tyrope.nl>
2022-01-25 18:35:54 +0100

    [saneParens]New type of contract found. SOLVER NOT IMPLEMENTED!

---
+++ contracts/saneParens.js
@@ -0,0 +1,17 @@
+/* Sanitize Parentheses in Expression
+Given a string remove the minimum number of invalid parentheses in order to validate the string.
+If there are multiple minimal ways to validate the string, provide all of the possible results.
+The answer should be provided as an array of strings.
+If it is impossible to validate the string the result should be an array with only an empty string.
+IMPORTANT: The string may contain letters, not just parentheses.
+*/
+
+/** @param {String} input */
+export function solver(input) {
+    // NOPE.
+}
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    ns.tprint(solver(ns.args[0]));
+}
