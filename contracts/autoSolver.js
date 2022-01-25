a7e407791a77b0ee80e107133c4b0fe5f0e50b29
Tyrope <github@tyrope.nl>
2022-01-25 22:47:36 +0100

    [autoSolver]Be a bit more vocal if we do an attempt, also enable AlgoStocksII

--- contracts/autoSolver.js
+++ contracts/autoSolver.js
@@ -3,11 +3,12 @@ import { getContracts } from '/contracts/locator.js';
 import { solver as algoStocksI } from '/contracts/algoStocksI.js';
 import { solver as algoStocksII } from '/contracts/algoStocksII.js';
 import { solver as arrayJumping } from '/contracts/arrayJumping.js';
+import { solver as findValidMath } from '/contracts/findValidMath.js';
 import { solver as generateIP } from '/contracts/generateIP.js';
 import { solver as minSumTriangle } from '/contracts/minSumTriangle.js';
-import { solver as uniquePathsGridI } from '/contracts/uniquePathsGridI.js';
 import { solver as saneParens } from '/contracts/saneParens.js';
 import { solver as spiralize } from '/contracts/spiralize.js';
+import { solver as uniquePathsGridI } from '/contracts/uniquePathsGridI.js';
 
 /** @param {NS} ns **/
 export async function main(ns) {
@@ -15,39 +16,53 @@ export async function main(ns) {
     for (let [file, srv] of CONTRACTS) {
         let type = ns.codingcontract.getContractType(file, srv);
         let input = ns.codingcontract.getData(file, srv);
-
+        let reward = false;
+        let func = null;
 
         switch (type) {
             case "Algorithmic Stock Trader I":
-                ns.codingcontract.attempt(algoStocksI(input), file, srv);
+                func = algoStocksI;
                 break;
             case "Algorithmic Stock Trader II":
-                //ns.codingcontract.attempt(algoStocksII(input), file, srv);
-                ns.tprint("algoStocksII solver not finished.");
+                func = algoStocksII;
                 break;
             case "Array Jumping Game":
-                ns.codingcontract.attempt(arrayJumping(input), file, srv);
+                func = arrayJumping;
+            case "Find All Valid Math Expressions":
+                //func = findValidMath;
+                ns.tprint("findValidMath solver not finished.");
                 break;
             case "Generate IP Addresses":
-                ns.codingcontract.attempt(generateIP(input), file, srv);
+                func = generateIP;
                 break;
             case "Minimum Path Sum in a Triangle":
-                ns.codingcontract.attempt(minSumTriangle(input), file, srv);
+                func = minSumTriangle;
                 break;
             case "Sanitize Parentheses in Expression":
-                //ns.codingcontract.attempt(saneParens(input), file, srv);
+                //func = saneParens;
                 ns.tprint("saneParens solver not finished.");
                 break;
             case "Spiralize Matrix":
-                //ns.codingcontract.attempt(spiralize(input), file, srv);
+                //func = spiralize;
                 ns.tprint("spiralize solver not finished.");
                 break;
             case "Unique Paths in a Grid I":
-                ns.codingcontract.attempt(uniquePathsGridI(input), file, srv);
+                func = uniquePathsGridI;
                 break;
             default:
-                let contractName = file.substr(0, file.length - 4);
-                ns.tprint(`No solver found for ${contractName}@${srv} (Type: ${type})`);
+                ns.tprint(`No solver found for ${file}@${srv} (Type: ${type})`);
+                break;
+        }
+        if (func != null) {
+            reward = ns.codingcontract.attempt(func(input), file, srv, { returnReward: true });
+            if (reward == "") {
+                ns.tprint(
+                    `ERROR: Failed ${file}@${srv} Type: ${type}` +
+                    ` (${ns.codingcontract.getNumTriesRemaining(file, srv)} tries remaining)`
+                );
+            } else {
+                ns.tprint(`SUCCESS: [${file}@${srv}]${reward} `);
+            }
         }
     }
 }
