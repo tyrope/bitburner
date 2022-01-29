299746cb6a05ac30df0514604ffc35658c0443da
Tyrope <github@tyrope.nl>
2022-01-29 23:52:38 +0100

    [totalWaysToSum]This works... but Maximum call stack size exceeded

--- contracts/totalWaysToSum.js
+++ contracts/totalWaysToSum.js
@@ -8,9 +8,31 @@ It is possible write four as a sum in exactly four different ways:
 How many different ways can the number n be written as a sum of at least two positive integers?
 */
 
-/** @param {Number[]} input */
+/**
+ * @param {Number} input
+ * @param {?Number[]} numSoFar
+ * @return {Number[][]}
+ */
+function getPossibleSums(input, numSoFar = Array()) {
+    if (input == 1) {
+        return [[1, ...numSoFar]];
+    }
+
+    let ret = Array();
+    ret.push([input, ...numSoFar]);
+    for (let i = input - 1; i > 0; i--) {
+        ret.push(...getPossibleSums(input - i, [i, ...numSoFar]));
+    }
+    return ret;
+}
+
+/** @param {Number} input */
 export function solver(input) {
-    // NOPE
+    let ret = new Set();
+    for (let sum of getPossibleSums(input)) {
+        ret.add(sum.sort().join('+'));
+    }
+    return [...ret].length;
 }
 
 /** @param {NS} ns **/
